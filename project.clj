49f29ac1a0a4eebd4a25fb24623465b210c0d964
James Larisch <jamesfordummies@gmail.com>
2015-12-20 11:16:36 -0500

    basic syntax definition complete

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "MIT"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [insaparse "1.4.1"]]
+                 [instaparse "1.4.1"]
+                 [org.clojure/core.match "0.3.0-alpha4"]]
   :main ^:skip-aot arraklisp.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/arraklisp/core.clj
+++ src/arraklisp/core.clj
@@ -1,14 +1,22 @@
 (ns arraklisp.core
-  (:require [instaparse.core :as insta])
+  (:require [instaparse.core :as insta]
+            [clojure.core.match :refer [match]])
   (:gen-class))
 
 (def to-syntax
   (insta/parser
-   "AL = NUM+ | '%' SYM+ | FUN | CALL
-    NUM = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
-    SYM = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z'
-    FUN = '(' SYM+ ' [' SYM+ '] ' AL ')'
-    CALL = '(' SYM+ ' ' (AL ' ')+ ')'"))
+   "AL = NUM | SYM | FUN | CALL | LET | DEF
+    NUM = ('0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9')+
+    SYM = ('a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z' | '+' | '-' | '\\\\' | '*')+
+    FUN = '(kwisatz {' (' ' SYM)* '} ' AL ')'
+    CALL = '(' SYM (' ' AL)* ')'
+    LET = '(muaddib [' ('(' SYM ' ' AL ')')* '] ' AL ')'
+    DEF = '(arrakis ' SYM ' ' AL ')'"))
+
+(defn parse
+  "Instaparse Syntax Tree -> Arraklisp Syntax Tree"
+  [tree]
+  (match tree
 
 
 
